f8a9748ef43a572afa9094c1d246979fd6d0eba9
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-29 22:53:08 +0200

    "Add face detection and image capture functionality for dataset creation"

--- dataset_creater.py
+++ dataset_creater.py
@@ -27,3 +27,20 @@ Age= input('Enter User Age')
 
 insertOrUpdate(Id, Name, Age)
 
+sampleNum=0
+
+while(True):
+    ret, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        sampleNum += 1
+        cv2.imwrite("dataSet/User."+str(Id)+"."+str(sampleNum)+".jpg", gray[y:y+h, x:x+w])
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.waitKey(100)
+    cv2.imshow('Face', img)
+    cv2.waitKey(1)
+    if(sampleNum>20):
+        break
+   
