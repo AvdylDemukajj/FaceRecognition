1bd82c602495f39dc8eba466c6cfacb47fe54a5d
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-29 23:08:19 +0200

    "updating code"

--- dataset_creater.py
+++ dataset_creater.py
@@ -5,44 +5,42 @@ import sqlite3
 faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cam = cv2.VideoCapture(0)
 
-def insertOrUpdate(Id, Name, Age):
-    conn = sqlite3.connect('FaceBase.db')
-    query = "SELECT * FROM Students WHERE Id ="+str(Id)
-    cursor = conn.execute(query)
-    isRecordExist = 0
-    for row in cursor:
-        isRecordExist = 1
-    if(isRecordExist==1):
-        conn.execute("Update Students SET Name=? WHERE Id=?",(Name, Id))
-        conn.execute("Update Students SET Age=? WHERE Id=?",(Age, Id))
+def insertOrUpdate(Name, Age):
+    conn = sqlite3.connect('sqlite.db')
+    cursor = conn.execute("SELECT Id FROM Students WHERE Name=? AND Age=?", (Name, Age))
+    data = cursor.fetchone()
+
+    if data:
+        Id = data[0]
     else:
-        conn.execute("INSERT INTO Students (Id, Name, Age) VALUES(?,?,?)",(Id, Name, Age))
+        conn.execute("INSERT INTO Students (Name, Age) VALUES(?,?)", (Name, Age))
+        Id = cursor.lastrowid
 
     conn.commit()
     conn.close()
+    return Id
 
-Id= input('Enter User Id')
-Name= input('Enter User Name')
-Age= input('Enter User Age')
+Name = input('Enter User Name: ')
+Age = input('Enter User Age: ')
 
-insertOrUpdate(Id, Name, Age)
+Id = insertOrUpdate(Name, Age)
 
-sampleNum=0
+sampleNum = 0
 
-while(True):
+while True:
     ret, img = cam.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceDetect.detectMultiScale(gray, 1.3, 5)
 
     for (x, y, w, h) in faces:
         sampleNum += 1
-        cv2.imwrite("dataSet/User."+str(Id)+"."+str(sampleNum)+".jpg", gray[y:y+h, x:x+w])
+        cv2.imwrite(f"dataSet/User.{Id}.{sampleNum}.jpg", gray[y:y+h, x:x+w])
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.waitKey(100)
     cv2.imshow('Face', img)
     cv2.waitKey(1)
-    if(sampleNum>20):
+    if sampleNum > 20:
         break
-   
+
 cam.release()
 cv2.destroyAllWindows()
