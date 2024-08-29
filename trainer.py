782e1f4f7733789508212beff7db9c448ddb6409
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-29 23:46:58 +0200

    "function that get images for training"

--- trainer.py
+++ trainer.py
@@ -11,4 +11,16 @@ path = "dataSet"
 def getImagesWithID(path):
     imagePaths = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".jpg")]
     faces = []
-    IDs = []
+    Ids = []
+
+    for imagePath in imagePaths:
+        faceImage = Image.open(imagePath).convert("L")
+        faceNp = np.array(faceImage, np.uint8)
+        Id = int(os.path.split(imagePath)[-1].split(".")[1])
+        print(Id)
+        faces.append(faceNp)
+        Ids.append(Id)
+        cv2.imshow("Training", faceNp)
+        cv2.waitKey(10)
+    return np.array(Ids), faces
+
