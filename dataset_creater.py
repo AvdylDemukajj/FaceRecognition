d82b479282b62a38202a2fe47ceb38a3931e3618
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-29 22:08:11 +0200

    "insert or update fuction"

--- dataset_creater.py
+++ dataset_creater.py
@@ -4,3 +4,19 @@ import sqlite3
 
 faceDetect = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cam = cv2.VideoCapture(0)
+
+def insertOrUpdate(Id, Name, Age):
+    conn = sqlite3.connect('FaceBase.db')
+    query = "SELECT * FROM Students WHERE Id ="+str(Id)
+    cursor = conn.execute(query)
+    isRecordExist = 0
+    for row in cursor:
+        isRecordExist = 1
+    if(isRecordExist==1):
+        conn.execute("Update Students SET Name=? WHERE Id=?",(Name, Id))
+        conn.execute("Update Students SET Age=? WHERE Id=?",(Age, Id))
+    else:
+        conn.execute("INSERT INTO Students (Id, Name, Age) VALUES(?,?,?)",(Id, Name, Age))
+
+    conn.commit()
+    conn.close()
