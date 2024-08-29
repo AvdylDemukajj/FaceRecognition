467581bb0aa5c4984b29cf8a28644d0276690026
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-30 00:05:43 +0200

    "get profile method"

--- detect.py
+++ detect.py
@@ -8,3 +8,13 @@ cam = cv2.VideoCapture(0)
 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 recognizer.read("recognizer/training.yml")
+
+def getprofile(id):
+    conn = sqlite3.connect("sqlite.db")
+    cursor = conn.execute("SELECT * FROM users WHERE Id=?", (id,))
+    profile = None
+
+    for row in cursor:
+        profile = row
+    conn.close()
+    return profile
